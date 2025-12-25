5daaf61a934ef8084a7f1374f00d402ec3855d94
vanshita <pamnanivanshita@gmail.com>
2025-12-25 23:18:52 +0530

    landing page done

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,19 +1,30 @@
 import "../styles/landing.css";
 
 import logo from "../assets/logo.png";
+import bgImage from "../assets/bgImage.png";
 
 function Landing() {
   return (
     <>
+      <img src={bgImage} className="bg-image" alt="" />
       <div className="logo">
         <img src={logo} alt="brewverzz logo" />
       </div>
 
       <div className="btn">
         <button> Login </button>
-
         <span>/</span>
-        <button> Sign in</button>
+        <button> Sign Up</button>
+      </div>
+
+      <div className="front">
+        <h1>BREWUVERZZ</h1>
+        <p>A Place Where Every Cups Tells The Story.</p>
+
+        <div className="action">
+          <button>Let's Explore</button>
+          <button>Quiz</button>
+        </div>
       </div>
     </>
   );
