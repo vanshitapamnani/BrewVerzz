dde239dcafefc9425f28ace3d835e0d618268f12
vanshita <pamnanivanshita@gmail.com>
2025-12-27 23:25:46 +0530

    Section added in Explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,10 +1,6 @@
 import ExploreCard from "../components/ExploreCard";
 import "../styles/explore.css";
 
-// import Guide from "../pages/BrewGuide";
-
-// import About from "../pages/AboutCoffee";
-// import Log from "../pages/CoffeeLog";
 import about from "../assets/about.png";
 import guide from "../assets/guide.png";
 import log from "../assets/log.png";
@@ -15,8 +11,12 @@ function Explore() {
         <h1 className="explore-title">Explore Brewuverzz</h1>
         <div className="explore-cards">
           <ExploreCard image={guide} title="Brew Guide" link="/guide" />
-          <ExploreCard image={about} title="Brew Guide" link="/about" />
-          <ExploreCard image={log} title="Brew Guide" link="/log" />
+          <ExploreCard image={about} title="More About Coffee" link="/about" />
+          <ExploreCard
+            image={log}
+            title="Log Your Coffee Experience"
+            link="/log"
+          />
         </div>
       </div>
     </div>
